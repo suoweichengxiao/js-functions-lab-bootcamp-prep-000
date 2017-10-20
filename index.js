3cc9c509eeea2336aa7e2f474f26db9e5a51d360
Joe Dettmar <joedettmar@gmail.com>
2017-10-20 16:45:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,3 @@
 function happyHolidays() {return "Happy holidays!"}
 function happyHolidaysTo(name) {return `Happy holidays, ${name}!`}
 function happyHolidayTo(holiday, name) {return `Happy ${holiday}, ${name}!`}
-function holidayCountdown(holiday, days) {return `It's ${days} until ${holiday}!`}
