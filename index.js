a3af0bf7c053b2b9f8f331191b61c867814aaccc
Joe Dettmar <joedettmar@gmail.com>
2017-10-20 16:29:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // write your code below!
 function happyHolidays() {return "Happy holidays!"}
-function happyHolidaysTo(name) {return "Happy holidays, ${name}!"}
+function happyHolidaysTo(name) {return "Happy holidays, ${name}"}
